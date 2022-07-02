1c364c6cdff90dcdb6ead95c709b169fb1dea706
Your Name <you@example.com>
2022-07-02 10:51:24 +0800

    fix error

--- steps/6-app.sh
+++ steps/6-app.sh
@@ -6,7 +6,7 @@ if [ -e $PROJ_OUT/app-release.apk ]; then
 else
  # libtainer
  #cp $PROJ_OUT/libtainer* app/src/main/assets/Toolkit/Preload/bin/
- cp $PROJ_ROOT/toolkit/toolkit.sh $PROJ_ROOT/App/app/src/main/assets/Toolkit/
+ cp $PROJ_ROOT/toolkit/toolkit.sh $PROJ_ROOT/app/app/src/main/assets/Toolkit/
  for arch in aarch64 arm x86_64; do
   if [[ "$arch" = "aarch64" ]];then
    unset arch
@@ -16,11 +16,11 @@ else
    unset arch
    export arch=x64
   fi
-  cp $PROJ_OUT/bin/$arch/* $PROJ_ROOT/App/app/src/main/assets/Toolkit/Preload/bin/$arch/
-  cp $PROJ_OUT/lib/$arch/libloader.so $PROJ_ROOT/App/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader.so
+  cp $PROJ_OUT/bin/$arch/* $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/bin/$arch/
+  cp $PROJ_OUT/lib/$arch/libloader.so $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader.so
   # for 64bit
   if [ -e $PROJ_OUT/lib/$arch/libloader32.so ]; then
-   cp $PROJ_OUT/lib/$arch/libloader32.so $PROJ_ROOT/App/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader32.so
+   cp $PROJ_OUT/lib/$arch/libloader32.so $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader32.so
   fi
  done
  chmod +x ./gradlew
