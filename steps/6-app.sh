76e638cc3075849b062131490c80965ef3a7c81b
Your Name <you@example.com>
2022-07-03 10:00:24 +0800

    add github ci build support

--- steps/6-app.sh
+++ steps/6-app.sh
@@ -4,9 +4,9 @@ build(){
 if [ -e $PROJ_OUT/app-release.apk ]; then
  echo 'skipping build'
 else
- # libtainer
- #cp $PROJ_OUT/libtainer* app/src/main/assets/Toolkit/Preload/bin/
- cp $PROJ_ROOT/toolkit/toolkit.sh $PROJ_ROOT/app/app/src/main/assets/Toolkit/
+ # 安装toolkit
+ cp $PROJ_ROOT/toolkit/toolkit.sh $PROJ_ROOT/app/app/src/main/assets/usr/
+ # 兼容app架构分类方式
  for arch in aarch64 arm x86_64; do
   if [[ "$arch" = "aarch64" ]];then
    mv $PROJ_OUT/bin/aarch64  $PROJ_OUT/bin/aa64
@@ -20,13 +20,15 @@ else
    unset arch
    export arch=x64
   fi
-  cp $PROJ_OUT/bin/$arch/* $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/bin/$arch/
-  cp $PROJ_OUT/lib/$arch/libloader.so $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader.so
+  # 安装运行环境相关
+  cp $PROJ_OUT/bin/$arch/* $PROJ_ROOT/app/app/src/main/assets/usr/preload/bin/$arch/
+  cp $PROJ_OUT/lib/$arch/libloader.so $PROJ_ROOT/app/app/src/main/assets/usr/preload/lib/$arch/libloader.so
   # for 64bit
   if [ -e $PROJ_OUT/lib/$arch/libloader32.so ]; then
-   cp $PROJ_OUT/lib/$arch/libloader32.so $PROJ_ROOT/app/app/src/main/assets/Toolkit/Preload/lib/$arch/libloader32.so
+    cp $PROJ_OUT/lib/$arch/libloader32.so $PROJ_ROOT/app/app/src/main/assets/usr/preload/lib/$arch/libloader32.so
   fi
  done
+ # 编译
  chmod +x ./gradlew
  ./gradlew build
  install -Dm644 app/build/outputs/apk/release/app-release.apk $PROJ_OUT/app-release.apk
